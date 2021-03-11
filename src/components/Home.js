ac3b23d0feb0c6a3fdf966960f2fd64450cbe102
samara1907 <o.mahmoud1342007@gmail.com>
2021-03-11 11:07:11 +0300

    deploy

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import {Container,Row,Col} from 'react-bootstrap';
 const Home=()=>{
     return(
         <>
-        <Container className='center'>
+        <Container className='p-top-50% center'>
         <Row>
             <Col><h1>Welcome to Hotels tool</h1></Col>
         </Row>
